69fc99c6b21e77e3188bd79b7423c0a7108ef4d9
tane-git <tane.alexander.wilson@gmail.com>
2022-03-24 15:09:10 +1300

    change constants to DEV

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -3,6 +3,9 @@
 
 export const IPFS_DEV = 'https://interipfs.thechaindata.com';
 export const IPFS_PROD = 'https://ipfs.subquery.network';
-export const IPFS_NODE_ENDPOINT = `${IPFS_PROD}/ipfs/api/v0`;
-export const IPFS_CLUSTER_ENDPOINT = `${IPFS_PROD}/cluster/add`;
+// CHANGE TO DEV ENVIRONMENT
+// export const IPFS_NODE_ENDPOINT = `${IPFS_PROD}/ipfs/api/v0`;
+export const IPFS_NODE_ENDPOINT = `${IPFS_DEV}/ipfs/api/v0`;
+// export const IPFS_CLUSTER_ENDPOINT = `${IPFS_PROD}/cluster/add`;
+export const IPFS_CLUSTER_ENDPOINT = `${IPFS_DEV}/cluster/add`;
 export const IPFS_REGEX = /^ipfs:\/\//i;
